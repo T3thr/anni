21d1485f042520834ca3db30c3add26a7c0cb233
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-02 03:14:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,78 @@
-document.getElementById('wish-form').addEventListener('submit', function(event) {
-  event.preventDefault();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const multer = require('multer');
+const admin = require('firebase-admin');
+const serviceAccount = require('./path/to/your/serviceAccountKey.json'); // Replace with your own service account key file
 
-  const name = document.getElementById('name').value;
-  const message = document.getElementById('message').value;
-  const pictureInput = document.getElementById('picture');
-  const pictureFile = pictureInput.files[0]; // Get the first selected file (if any)
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-  if (message.trim() !== '') {
-    const newWish = {
-      name: name || 'Anonymous',
-      message,
-      pictureURL: null
-    };
+const db = admin.firestore();
+const storage = admin.storage().bucket();
 
-    if (pictureFile) {
-      const pictureReader = new FileReader();
-      pictureReader.onload = function() {
-        const pictureURL = pictureReader.result;
-        newWish.pictureURL = pictureURL;
-        saveWish(newWish);
-        showThankYouAlert();
-      };
-      pictureReader.readAsDataURL(pictureFile);
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+// Endpoint to save a wish
+app.post('/saveWish', upload.single('picture'), async (req, res) => {
+  try {
+    const { name, message } = req.body;
+    const wish = { name, message, timestamp: Date.now() };
+
+    if (req.file) {
+      const pictureName = `${Date.now()}_${req.file.originalname}`;
+      const file = storage.file(pictureName);
+      const fileStream = file.createWriteStream({
+        metadata: { contentType: req.file.mimetype }
+      });
+
+      fileStream.on('error', (err) => {
+        console.error('Error uploading picture:', err);
+        res.status(500).send('Error uploading picture');
+      });
+
+      fileStream.on('finish', () => {
+        wish.pictureURL = `https://storage.googleapis.com/${storage.name}/${pictureName}`;
+        saveWishToFirestore(wish);
+      });
+
+      fileStream.end(req.file.buffer);
     } else {
-      saveWish(newWish);
-      showThankYouAlert();
+      saveWishToFirestore(wish);
     }
 
-    // Clear input fields after submission
-    document.getElementById('name').value = '';
-    document.getElementById('message').value = '';
-    pictureInput.value = '';
+    res.status(200).send('Wish saved successfully');
+  } catch (error) {
+    console.error('Error saving wish:', error);
+    res.status(500).send('Error saving wish');
   }
 });
 
-function showThankYouAlert() {
-  alert('Thank you for your wishes!');
+// Function to save wish to Firestore
+async function saveWishToFirestore(wish) {
+  await db.collection('wishes').add(wish);
 }
 
-function saveWish(newWish) {
-  let wishes = JSON.parse(localStorage.getItem('wishes'));
-  if (!wishes) {
-    wishes = [];
+// Endpoint to fetch all wishes
+app.get('/getAllWishes', async (req, res) => {
+  try {
+    const snapshot = await db.collection('wishes').orderBy('timestamp', 'desc').get();
+    const wishes = snapshot.docs.map(doc => doc.data());
+    res.status(200).json(wishes);
+  } catch (error) {
+    console.error('Error fetching wishes:', error);
+    res.status(500).send('Error fetching wishes');
   }
-  wishes.push(newWish);
-  localStorage.setItem('wishes', JSON.stringify(wishes));
-}
+});
+
+// Start the server
+const port = 5000; // Change to the desired port
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
