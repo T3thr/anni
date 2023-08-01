f4372e342be6fd75a5f1cf21024488409560bdfe
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-01 23:18:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,33 @@ document.getElementById('wish-form').addEventListener('submit', function(event) 
   const pictureFile = pictureInput.files[0]; // Get the first selected file (if any)
 
   if (message.trim() !== '') {
-    const wish = {
-      name: name || 'Anonymous',
-      message,
-      pictureFile
-    };
+    const wishList = document.getElementById('wish-list');
+    const newWish = document.createElement('div');
+    newWish.classList.add('wish');
 
-    saveWish(wish);
-    showThankYouAlert();
+    let wishContent = '';
+    if (name !== '') {
+      wishContent += `<strong>${name}:</strong> `;
+    }
+    wishContent += message;
+
+    if (pictureFile) {
+      const pictureReader = new FileReader();
+      pictureReader.onload = function() {
+        const pictureURL = pictureReader.result;
+        wishContent += `<br><img src="${pictureURL}" alt="Wish Picture">`;
+        newWish.innerHTML = wishContent;
+        wishList.appendChild(newWish);
+        showThankYouAlert();
+        saveWish(name, message, pictureFile);
+      };
+      pictureReader.readAsDataURL(pictureFile);
+    } else {
+      newWish.innerHTML = wishContent;
+      wishList.appendChild(newWish);
+      showThankYouAlert();
+      saveWish(name, message);
+    }
 
     // Clear input fields after submission
     document.getElementById('name').value = '';
@@ -27,7 +46,13 @@ function showThankYouAlert() {
   alert('Thank you for your wishes!');
 }
 
-function saveWish(wish) {
+function saveWish(name, message, pictureFile) {
+  const wish = {
+    name: name || 'Anonymous',
+    message,
+    pictureFile
+  };
+
   let wishes = localStorage.getItem('wishes');
   if (wishes) {
     wishes = JSON.parse(wishes);
