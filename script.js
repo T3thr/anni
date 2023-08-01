44d5dc1916be85e0fc44fa2889330407539d9b0c
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-01 20:15:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Personal Access Token from GitHub (Replace with your token)
-const accessToken = 'KAITUNG';
+const API_BASE_URL = 'http://localhost:3000/api'; // Replace with your server's URL
 
 document.getElementById('wish-form').addEventListener('submit', async function(event) {
   event.preventDefault();
@@ -12,22 +11,19 @@ document.getElementById('wish-form').addEventListener('submit', async function(e
   if (message.trim() !== '') {
     const wish = {
       name: name || 'Anonymous',
-      message,
-      pictureURL: null // We'll save the image to Gist instead of local storage
+      message
     };
 
     if (pictureFile) {
       try {
-        const gistId = await createGist(pictureFile);
-        wish.pictureURL = `https://gist.github.com/${gistId}.png`;
+        const pictureURL = await uploadPictureToServer(pictureFile);
+        wish.pictureURL = pictureURL;
       } catch (error) {
         console.error('Error uploading picture:', error);
       }
     }
 
-    let wishes = JSON.parse(localStorage.getItem('wishes')) || [];
-    wishes.push(wish);
-    localStorage.setItem('wishes', JSON.stringify(wishes));
+    await saveWishToServer(wish);
 
     // Clear input fields after submission
     document.getElementById('name').value = '';
@@ -35,41 +31,48 @@ document.getElementById('wish-form').addEventListener('submit', async function(e
     pictureInput.value = '';
 
     showThankYouAlert();
-    displayWishes(); // Display the updated wishes immediately
   }
 });
 
-async function createGist(pictureFile) {
-  // Same as before
+async function uploadPictureToServer(pictureFile) {
+  const formData = new FormData();
+  formData.append('picture', pictureFile);
+
+  try {
+    const response = await fetch(`${API_BASE_URL}/upload`, {
+      method: 'POST',
+      body: formData
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return data.pictureURL;
+    } else {
+      throw new Error('Failed to upload picture to server.');
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function saveWishToServer(wish) {
+  try {
+    const response = await fetch(`${API_BASE_URL}/wishes`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(wish)
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to save wish to server.');
+    }
+  } catch (error) {
+    throw error;
+  }
 }
 
 function showThankYouAlert() {
   alert('Thank you for your wishes!');
 }
-
-function displayWishes() {
-  const wishList = document.getElementById('wish-list');
-  const wishes = JSON.parse(localStorage.getItem('wishes')) || [];
-
-  wishList.innerHTML = ''; // Clear existing wishes
-
-  if (wishes.length > 0) {
-    wishes.forEach((wish) => {
-      const wishCard = document.createElement('div');
-      wishCard.classList.add('wish');
-      wishCard.innerHTML = `
-        <p><strong>${wish.name}:</strong> ${wish.message}</p>
-        ${wish.pictureURL ? `<img src="${wish.pictureURL}" alt="Wish Picture">` : ''}
-      `;
-      wishList.appendChild(wishCard);
-    });
-  } else {
-    const noWishesMessage = document.createElement('p');
-    noWishesMessage.textContent = 'No wishes yet. Be the first to leave a wish!';
-    wishList.appendChild(noWishesMessage);
-  }
-}
-
-displayWishes(); // Display the initial wishes when the page loads
-
-// The rest of the code remains the same
