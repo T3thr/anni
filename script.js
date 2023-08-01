0bc975e14bf1cea74656b1f2eed23b8362058e19
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-01 18:05:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
-const messageForm = document.getElementById('message-form');
-const wishesList = document.getElementById('wishes-list');
+// Function to save a wish to the GitHub repository
+async function saveWishToGitHub(name, message) {
+    const data = {
+        name: name,
+        message: message
+    };
 
-messageForm.addEventListener('submit', function (event) {
-    event.preventDefault();
+    const response = await fetch('https://api.github.com/repos/T3thr/anniversary-wishes/contents/wishes/' + Date.now() + '.json', {
+        method: 'PUT',
+        headers: {
+            'Authorization': 'Bearer YOUR_GITHUB_ACCESS_TOKEN',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            message: 'Create new wish',
+            content: btoa(JSON.stringify(data))
+        })
+    });
 
-    const nameInput = document.getElementById('name');
-    const messageInput = document.getElementById('message');
-    const pictureInput = document.getElementById('picture');
-
-    const name = nameInput.value.trim();
-    const message = messageInput.value.trim();
-
-    if (!message) {
-        alert('Please enter a message before submitting.');
-        return;
+    if (response.ok) {
+        console.log('Wish saved to GitHub!');
+    } else {
+        console.error('Error saving wish to GitHub:', response.status, response.statusText);
     }
+}
 
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `<strong>${name ? name + ': ' : ''}</strong>${message}`;
-    wishesList.appendChild(listItem);
+// Function to load wishes from the GitHub repository and display on the second page
+async function loadWishesFromGitHub() {
+    const response = await fetch('https://api.github.com/repos/T3thr/anniversary-wishes/contents/wishes', {
+        headers: {
+            'Authorization': 'Bearer KAITUNG'
+        }
+    });
 
-    messageInput.value = '';
-    nameInput.value = '';
-});
+    if (response.ok) {
+        const wishes = await response.json();
+        const wishesList = document.getElementById('wishes-list');
+        for (const wish of wishes) {
+            const wishData = JSON.parse(atob(wish.content));
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `<strong>${wishData.name ? wishData.name + ': ' : ''}</strong>${wishData.message}`;
+            wishesList.appendChild(listItem);
+        }
+    } else {
+        console.error('Error loading wishes from GitHub:', response.status, response.statusText);
+    }
+}
+
+// Load wishes when the second page loads
+if (window.location.pathname === '/wishes.html') {
+    loadWishesFromGitHub();
+}
