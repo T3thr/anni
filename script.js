d0e39b392b5934f5aa55d857b88381f7df4cc1d4
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-01 23:35:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCS6hFxkfaqyTWvO7Zlo23zDbAWh8U3Oc",
+  authDomain: "anni-e336f.firebaseapp.com",
+  databaseURL: "https://anni-e336f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "anni-e336f",
+  storageBucket: "anni-e336f.appspot.com",
+  messagingSenderId: "773257785211",
+  appId: "1:773257785211:web:7735061a858604eb7757de",
+  measurementId: "G-WME9JEGD0R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// ... Rest of the script.js code ...
+
+
 document.getElementById('wish-form').addEventListener('submit', function(event) {
   event.preventDefault();
 
@@ -50,8 +75,30 @@ function saveWish(name, message, pictureFile) {
   const wish = {
     name: name || 'Anonymous',
     message,
-    pictureFile
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
   };
+  
+  // Save the wish to Firestore
+  db.collection('wishes')
+    .add(wish)
+    .then((docRef) => {
+      if (pictureFile) {
+        uploadPicture(docRef.id, pictureFile)
+          .then(() => {
+            showThankYouAlert();
+          })
+          .catch(error => {
+            console.error('Failed to upload picture:', error);
+            showThankYouAlert();
+          });
+      } else {
+        showThankYouAlert();
+      }
+    })
+    .catch(error => {
+      console.error('Error saving wish:', error);
+    });
+}
 
   let wishes = localStorage.getItem('wishes');
   if (wishes) {
@@ -61,4 +108,18 @@ function saveWish(name, message, pictureFile) {
     wishes = [wish];
   }
   localStorage.setItem('wishes', JSON.stringify(wishes));
+}
+
+function uploadPicture(docId, pictureFile) {
+  return new Promise((resolve, reject) => {
+    const storageRef = firebase.storage().ref(`wishes/${docId}/${pictureFile.name}`);
+    const uploadTask = storageRef.put(pictureFile);
+
+    uploadTask.on(
+      'state_changed',
+      null,
+      error => reject(error),
+      () => resolve()
+    );
+  });
 }
