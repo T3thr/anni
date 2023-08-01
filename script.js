8fd1eb78ac69f469a59fcba089c57ddbe4537df3
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-01 17:25:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
-// Function to fetch and display all wishes from wishes.json
-function fetchAndDisplayWishes() {
-  fetch('https://raw.githubusercontent.com/T3thr/anni/main/wishes.json')
-    .then(response => response.json())
-    .then(data => {
-      const wishes = data;
-      const wishList = document.getElementById('wish-list');
+document.getElementById('wish-form').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-      if (wishes && wishes.length > 0) {
-        wishes.forEach((wish) => {
-          const wishCard = document.createElement('div');
-          wishCard.classList.add('wish');
-          wishCard.innerHTML = `
-            <p><strong>${wish.name}:</strong> ${wish.message}</p>
-            ${wish.pictureURL ? `<img src="${wish.pictureURL}" alt="Wish Picture">` : ''}
-          `;
-          wishList.appendChild(wishCard);
-        });
-      } else {
-        const noWishesMessage = document.createElement('p');
-        noWishesMessage.textContent = 'No wishes yet. Be the first to leave a wish!';
-        wishList.appendChild(noWishesMessage);
-      }
-    })
-    .catch(error => console.error('Error fetching wishes data:', error));
+  const name = document.getElementById('name').value;
+  const message = document.getElementById('message').value;
+  const pictureInput = document.getElementById('picture');
+  const pictureFile = pictureInput.files[0]; // Get the first selected file (if any)
+
+  if (message.trim() !== '') {
+    const wishList = document.getElementById('wish-list');
+    const newWish = document.createElement('div');
+    newWish.classList.add('wish');
+
+    let wishContent = '';
+    if (name !== '') {
+      wishContent += `<strong>${name}:</strong> `;
+    }
+    wishContent += message;
+
+    if (pictureFile) {
+      const pictureReader = new FileReader();
+      pictureReader.onload = function() {
+        const pictureURL = pictureReader.result;
+        wishContent += `<br><img src="${pictureURL}" alt="Wish Picture">`;
+        newWish.innerHTML = wishContent;
+        wishList.appendChild(newWish);
+        showThankYouAlert();
+
+        // Save the wish to the repository
+        saveWishToRepository(name, message, pictureFile);
+      };
+      pictureReader.readAsDataURL(pictureFile);
+    } else {
+      newWish.innerHTML = wishContent;
+      wishList.appendChild(newWish);
+      showThankYouAlert();
+
+      // Save the wish to the repository
+      saveWishToRepository(name, message);
+    }
+
+    // Clear input fields after submission
+    document.getElementById('name').value = '';
+    document.getElementById('message').value = '';
+    pictureInput.value = '';
+  }
+});
+
+function showThankYouAlert() {
+  alert('Thank you for your wishes!');
 }
 
-// Function to save the wish to wishes.json
 function saveWishToRepository(name, message, pictureFile) {
   const wish = {
+    id: Date.now().toString(), // Unique identifier for each wish (using timestamp)
     name: name || 'Anonymous',
     message,
     pictureURL: pictureFile ? URL.createObjectURL(pictureFile) : null
@@ -41,22 +66,3 @@ function saveWishToRepository(name, message, pictureFile) {
       return fetch('https://api.github.com/repos/T3thr/anni/contents/wishes.json', {
         method: 'PUT',
         headers: {
-          'Authorization': 'Bearer KAITUNG',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          message: 'Update wishes.json',
-          content: btoa(JSON.stringify(currentWishes, null, 2)),
-          sha: data?.sha || null
-        })
-      });
-    })
-    .then(() => {
-      // Refresh the wishes display after saving the new wish
-      fetchAndDisplayWishes();
-    })
-    .catch(error => console.error('Error saving wish to repository:', error));
-}
-
-// Call fetchAndDisplayWishes() to display existing wishes on page load
-fetchAndDisplayWishes();
