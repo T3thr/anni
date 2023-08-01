8982a99ea4c18d6ae7e602a6906e34673f395a66
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-01 23:09:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,37 +4,19 @@ document.getElementById('wish-form').addEventListener('submit', function(event) 
   const name = document.getElementById('name').value;
   const message = document.getElementById('message').value;
   const pictureInput = document.getElementById('picture');
-  const pictureFile = pictureInput.files[0];
+  const pictureFile = pictureInput.files[0]; // Get the first selected file (if any)
 
   if (message.trim() !== '') {
-    const wishList = document.getElementById('wish-list');
-    const newWish = document.createElement('div');
-    newWish.classList.add('wish');
+    const wish = {
+      name: name || 'Anonymous',
+      message,
+      pictureFile
+    };
 
-    let wishContent = '';
-    if (name !== '') {
-      wishContent += `<strong>${name}:</strong> `;
-    }
-    wishContent += message;
+    saveWish(wish);
+    showThankYouAlert();
 
-    if (pictureFile) {
-      const pictureReader = new FileReader();
-      pictureReader.onload = function() {
-        const pictureURL = pictureReader.result;
-        wishContent += `<br><img src="${pictureURL}" alt="Wish Picture">`;
-        newWish.innerHTML = wishContent;
-        wishList.appendChild(newWish);
-        showThankYouAlert();
-        saveWish(name, message, pictureFile);
-      };
-      pictureReader.readAsDataURL(pictureFile);
-    } else {
-      newWish.innerHTML = wishContent;
-      wishList.appendChild(newWish);
-      showThankYouAlert();
-      saveWish(name, message);
-    }
-
+    // Clear input fields after submission
     document.getElementById('name').value = '';
     document.getElementById('message').value = '';
     pictureInput.value = '';
@@ -45,13 +27,7 @@ function showThankYouAlert() {
   alert('Thank you for your wishes!');
 }
 
-function saveWish(name, message, pictureFile) {
-  const wish = {
-    name: name || 'Anonymous',
-    message,
-    pictureFile
-  };
-
+function saveWish(wish) {
   let wishes = localStorage.getItem('wishes');
   if (wishes) {
     wishes = JSON.parse(wishes);
