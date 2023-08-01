7fb9c06335b6c6d71d21b8a18a4b906a86f98c2c
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-01 16:01:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+document.getElementById('wish-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const name = document.getElementById('name').value;
+    const message = document.getElementById('message').value;
+    const pictureInput = document.getElementById('picture');
+    const pictureFile = pictureInput.files[0]; // Get the first selected file (if any)
+  
+    if (message.trim() !== '') {
+      const wishList = document.getElementById('wish-list');
+      const newWish = document.createElement('div');
+      newWish.classList.add('wish');
+  
+      let wishContent = '';
+      if (name !== '') {
+        wishContent += `<strong>${name}:</strong> `;
+      }
+      wishContent += message;
+  
+      if (pictureFile) {
+        const pictureReader = new FileReader();
+        pictureReader.onload = function() {
+          const pictureURL = pictureReader.result;
+          wishContent += `<br><img src="${pictureURL}" alt="Wish Picture">`;
+          newWish.innerHTML = wishContent;
+          wishList.appendChild(newWish);
+          showThankYouAlert();
+          saveWish(name, message, pictureFile);
+        };
+        pictureReader.readAsDataURL(pictureFile);
+      } else {
+        newWish.innerHTML = wishContent;
+        wishList.appendChild(newWish);
+        showThankYouAlert();
+        saveWish(name, message);
+      }
+  
+      // Clear input fields after submission
+      document.getElementById('name').value = '';
+      document.getElementById('message').value = '';
+      pictureInput.value = '';
+    }
+  });
+  
+  function showThankYouAlert() {
+    alert('Thank you for your wishes!');
+  }
+  
+  function saveWish(name, message, pictureFile) {
+    const wish = {
+      name: name || 'Anonymous',
+      message,
+      pictureURL: pictureFile ? URL.createObjectURL(pictureFile) : null
+    };
+  
+    let wishes = localStorage.getItem('wishes');
+    if (wishes) {
+      wishes = JSON.parse(wishes);
+      wishes.push(wish);
+    } else {
+      wishes = [wish];
+    }
+    localStorage.setItem('wishes', JSON.stringify(wishes));
+  }
+  
