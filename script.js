7e0aa9a59d96c02b15a3d992ce9669639ead7fc6
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-01 18:26:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,17 @@
-// Function to fetch and display all wishes from wishes.json
-function fetchAndDisplayWishes() {
-  fetch('https://raw.githubusercontent.com/T3thr/anni/main/wishes.json')
-    .then(response => response.json())
-    .then(data => {
-      const wishes = data;
-      const wishList = document.getElementById('wish-list');
-
-      if (wishes && wishes.length > 0) {
-        wishes.forEach((wish) => {
-          const wishCard = document.createElement('div');
-          wishCard.classList.add('wish');
-          wishCard.innerHTML = `
-            <p><strong>${wish.name}:</strong> ${wish.message}</p>
-            ${wish.pictureURL ? `<img src="${wish.pictureURL}" alt="Wish Picture">` : ''}
-          `;
-          wishList.appendChild(wishCard);
-        });
-      } else {
-        const noWishesMessage = document.createElement('p');
-        noWishesMessage.textContent = 'No wishes yet. Be the first to leave a wish!';
-        wishList.appendChild(noWishesMessage);
-      }
-    })
-    .catch(error => console.error('Error fetching wishes data:', error));
-}
-
 // Function to save the wish to wishes.json
 function saveWishToRepository(name, message, pictureFile) {
   const wish = {
+    id: Date.now().toString(), // Unique identifier for each wish (using timestamp)
     name: name || 'Anonymous',
     message,
     pictureURL: pictureFile ? URL.createObjectURL(pictureFile) : null
-	@@ -41,22 +66,3 @@ function saveWishToRepository(name, message, pictureFile) {
+  };
+
+  fetch('https://raw.githubusercontent.com/T3thr/anni/main/wishes.json')
+    .then(response => response.json())
+    .then(data => {
+      const currentWishes = data || [];
+      currentWishes.push(wish);
       return fetch('https://api.github.com/repos/T3thr/anni/contents/wishes.json', {
         method: 'PUT',
         headers: {
@@ -46,11 +26,8 @@ function saveWishToRepository(name, message, pictureFile) {
       });
     })
     .then(() => {
-      // Refresh the wishes display after saving the new wish
-      fetchAndDisplayWishes();
+      // Redirect to wishes.html to see all wishes after saving the new wish
+      window.location.href = 'wishes.html';
     })
     .catch(error => console.error('Error saving wish to repository:', error));
 }
-
-// Call fetchAndDisplayWishes() to display existing wishes on page load
-fetchAndDisplayWishes();
