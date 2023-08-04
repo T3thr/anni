65fb4770667fdc61e467a525b1d343775a7f0a8a
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-04 17:41:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,51 @@
-// Initialize Firebase (Replace with your Firebase config)
-const firebaseConfig = {
-  apiKey: "AIzaSyDCS6hFxkfaqyTWvO7Zlo23zDbAWh8U3Oc",
-  authDomain: "anni-e336f.firebaseapp.com",
-  databaseURL: "https://anni-e336f-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "anni-e336f",
-  storageBucket: "anni-e336f.appspot.com",
-  messagingSenderId: "773257785211",
-  appId: "1:773257785211:web:7735061a858604eb7757de",
-  measurementId: "G-WME9JEGD0R"
-};
-firebase.initializeApp(firebaseConfig);
+document.addEventListener("DOMContentLoaded", function() {
+    const crushNameElement = document.getElementById("crushName");
+    const messageForm = document.getElementById("messageForm");
+    const wishesContainer = document.getElementById("wishes");
 
-const db = firebase.firestore();
-
-document.getElementById('wish-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const name = document.getElementById('name').value;
-  const message = document.getElementById('message').value;
-  const pictureInput = document.getElementById('picture');
-  const pictureFile = pictureInput.files[0]; // Get the first selected file (if any)
-
-  if (message.trim() !== '') {
-    const newWish = {
-      name: name || 'Anonymous',
-      message,
-      pictureURL: null
-    };
-
-    if (pictureFile) {
-      const pictureReader = new FileReader();
-      pictureReader.onload = function() {
-        const pictureURL = pictureReader.result;
-        newWish.pictureURL = pictureURL;
-        saveWish(newWish);
-        showThankYouAlert();
-      };
-      pictureReader.readAsDataURL(pictureFile);
-    } else {
-      saveWish(newWish);
-      showThankYouAlert();
-    }
-
-    // Clear input fields after submission
-    document.getElementById('name').value = '';
-    document.getElementById('message').value = '';
-    pictureInput.value = '';
-  }
-});
-
-function showThankYouAlert() {
-  alert('Thank you for your wishes!');
-}
-
-// Save wish to Firebase Firestore
-function saveWish(newWish) {
-  const db = firebase.firestore();
-  db.collection('wishes')
-    .add(newWish)
-    .catch(error => {
-      console.error('Error saving wish:', error);
+    // Initialize Firebase
+    firebase.initializeApp({
+        apiKey: "AIzaSyDCS6hFxkfaqyTWvO7Zlo23zDbAWh8U3Oc",
+        authDomain: "[YOUR_PROJECT_ID].anni-e336f.firebaseapp.com",
+        projectId: "anni-e336f"
     });
-  
-}
 
+    const db = firebase.firestore();
 
+    // Replace [Your Crush's Name] with the actual name
+    crushNameElement.textContent = "[Your Crush's Name]";
+
+    messageForm.addEventListener("submit", async function(event) {
+        event.preventDefault();
+
+        const nameInput = document.getElementById("name");
+        const messageInput = document.getElementById("message");
+
+        const name = nameInput.value || "Anonymous";
+        const message = messageInput.value;
+
+        // Store wish in Firestore
+        await db.collection("wishes").add({
+            name: name,
+            message: message,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        });
+
+        // Clear form inputs
+        nameInput.value = "";
+        messageInput.value = "";
+    });
+
+    // Load wishes from Firestore
+    db.collection("wishes").orderBy("timestamp", "desc").onSnapshot(snapshot => {
+        wishesContainer.innerHTML = ""; // Clear existing wishes
+
+        snapshot.forEach(doc => {
+            const wish = doc.data();
+            const messageElement = document.createElement("div");
+            messageElement.classList.add("message");
+            messageElement.innerHTML = `<strong>${wish.name}</strong>: ${wish.message}`;
+            wishesContainer.appendChild(messageElement);
+        });
+    });
+});
