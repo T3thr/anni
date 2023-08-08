e1e1d7af7540566857106d53dc0708107cbf06a8
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-08 19:01:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   const db = firebase.firestore();
+  const auth = firebase.auth();
 
   submitButton.addEventListener("click", function () {
     const commentText = commentInput.value;
@@ -19,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
       db.collection("comments").add({
         text: commentText,
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        userId: auth.currentUser ? auth.currentUser.uid : null,
       });
 
       commentInput.value = "";
@@ -34,7 +36,30 @@ document.addEventListener("DOMContentLoaded", function () {
         const commentItem = document.createElement("li");
         commentItem.className = "comment";
         commentItem.textContent = doc.data().text;
+
+        // Show delete button if the current user is the owner
+        if (auth.currentUser && doc.data().userId === auth.currentUser.uid) {
+          const deleteButton = document.createElement("button");
+          deleteButton.textContent = "Delete";
+          deleteButton.addEventListener("click", function () {
+            db.collection("comments").doc(doc.id).delete();
+          });
+          commentItem.appendChild(deleteButton);
+        }
+
         commentList.appendChild(commentItem);
       });
     });
+
+  // User authentication state change listener
+  auth.onAuthStateChanged(function (user) {
+    // Show/hide comment input and submit button based on authentication
+    if (user) {
+      commentInput.style.display = "block";
+      submitButton.style.display = "block";
+    } else {
+      commentInput.style.display = "none";
+      submitButton.style.display = "none";
+    }
+  });
 });
