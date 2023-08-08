64df73ffe5ecb9d14a30363314381c35c0207e8c
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-09 00:18:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const commentText = commentInput.value;
     if (commentText !== "") {
       // Add comment to Firestore
-      db.collection("comments").add({
+      db.collection("คุณคิดเห็นอย่างไรกับวันอาเซียน 8.8").add({
         text: commentText,
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
       });
@@ -25,13 +25,13 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Listen for changes in the comments collection and update the UI
-  db.collection("comments")
+  db.collection("คุณคิดเห็นอย่างไรกับวันอาเซียน 8.8")
     .orderBy("timestamp")
     .onSnapshot(function (snapshot) {
       commentList.innerHTML = "";
       snapshot.forEach(function (doc) {
         const commentItem = document.createElement("li");
-        commentItem.className = "comment";
+        commentItem.className = "คุณคิดเห็นอย่างไรกับวันอาเซียน 8.8";
         commentItem.textContent = doc.data().text;
         commentList.appendChild(commentItem);
       });
